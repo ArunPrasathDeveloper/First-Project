993fa4542360018f108c0b6fd4e438d822179628
ArunPrasathDeveloper <arunprasathsuba@gmail.com>
2025-12-06 19:18:20 +0530

    Change variable a to 1

--- Addition.java
+++ Addition.java
@@ -2,7 +2,7 @@ class Addition
 {
     public class static void main(String [] args)
     {
-        int a=10,b=20,c;
+        int a=1,b=20,c;
         c=a+b;
         System.out.println("C: "+c);
     }
